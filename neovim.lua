4736bdb88091bbf72342ca7c5cdda4d0f1ed9738
J. S. Brown <jsbrown7@gmail.com>
2026-02-25 22:09:47 -0800

    Added loading of official retro-82.nvim colorscheme

--- neovim.lua
+++ neovim.lua
@@ -7,58 +7,21 @@
 --
 
 return {
+	-- Load the Retro-82 colorscheme plugin immediately so it wins startup theme selection.
 	{
-		"bjarneo/aether.nvim",
-		branch = "v2",
-		name = "aether",
+		"oldjobobo/retro-82.nvim",
+		lazy = false,
 		priority = 1000,
-		opts = {
-			transparent = false,
-			colors = {
-				-- Background colors
-				bg = "#00172e",
-				bg_dark = "#00172e",
-				bg_highlight = "#134e5a",
-
-				-- Foreground colors
-				-- fg: Object properties, builtin types, builtin variables, member access, default text
-				fg = "#f6dcac",
-				-- fg_dark: Inactive elements, statusline, secondary text
-				fg_dark = "#a7c9c6",
-				-- comment: Line highlight, gutter elements, disabled states
-				comment = "#134e5a",
-
-				-- Accent colors
-				-- red: Errors, diagnostics, tags, deletions, breakpoints
-				red = "#f85525",
-				-- orange: Constants, numbers, current line number, git modifications
-				orange = "#f85525",
-				-- yellow: Types, classes, constructors, warnings, numbers, booleans
-				yellow = "#e97b3c",
-				-- green: Comments, strings, success states, git additions
-				green = "#028391",
-				-- cyan: Parameters, regex, preprocessor, hints, properties
-				cyan = "#8cbfb8",
-				-- blue: Functions, keywords, directories, links, info diagnostics
-				blue = "#faa968",
-				-- purple: Storage keywords, special keywords, identifiers, namespaces
-				purple = "#3f8f8a",
-				-- magenta: Function declarations, exception handling, tags
-				magenta = "#3f8f8a",
-			},
-		},
-		config = function(_, opts)
-			require("aether").setup(opts)
-			vim.cmd.colorscheme("aether")
-
-			-- Enable hot reload
-			require("aether.hotreload").setup()
+		config = function()
+			-- Apply the colorscheme during plugin config.
+			vim.cmd("colorscheme retro-82")
 		end,
 	},
+	-- Keep LazyVim aligned with the same scheme name.
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "aether",
+			colorscheme = "retro-82",
 		},
 	},
 }
@@ -77,4 +40,3 @@ return {
 --    \: \  \ \  \:\_\ \ \\::(_)  \ \\:\_\ \ \\::(_)  \ \\:\_\ \ \
 --     \_____\/   \_____\/ \_______\/ \_____\/ \_______\/ \_____\/
 --
-
